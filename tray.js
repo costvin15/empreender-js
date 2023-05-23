c4d1e613016472966fa451df65d1e5846b479071
Vinicius Castro <costacastrovinicius7@gmail.com>
2023-05-23 15:20:18 -0300

    Tray script update

--- tray.js
+++ tray.js
@@ -4,4 +4,4 @@ var thisHeading = headings.iterateNext();
 console.log(thisHeading);
 console.log(thisHeading.textContent);
 
-thisHeading.innerHTML = '<div id="rastreioDiv"></div><script type="text/javascript" id="rastreioScript" data-user="J24WPnA" src="https://empreender.nyc3.cdn.digitaloceanspaces.com/static/RASTREIOPUB/rastreio.js"></script>';
+thisHeading.innerHTML = '<div id="rastreioDiv"></div>';
